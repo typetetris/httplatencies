d2572e966b774998290bf4593698d091d61a9220
Eric Wolf <eric.wolf@lindenbaum.eu>
2021-07-21 07:39:15 +0200

    Adjust run.sh

--- run.sh
+++ run.sh
@@ -2,17 +2,19 @@
 
 target/release/httplatencies \
   --urls \
-      http://127.0.0.6:8080/noop \
       http://127.0.0.2:8080/noop \
       http://127.0.0.3:8080/noop \
       http://127.0.0.4:8080/noop \
       http://127.0.0.5:8080/noop \
+      http://127.0.0.6:8080/noop \
+      http://127.0.0.7:8080/noop \
   --local-ips \
-      127.0.100.6 \
       127.0.100.2 \
       127.0.100.3 \
       127.0.100.4 \
       127.0.100.5 \
+      127.0.100.6 \
+      127.0.100.7 \
   --task-count $1 \
   --probe-count $2 \
-  --clients $3
+  --runtime-count $3
